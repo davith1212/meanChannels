e247d6e8b87cfb0c15d40ade2453f7965262ed70
A Libunao <oldnao@gmail.com>
2016-01-03 21:55:03 -0800

    ADDED FILE: to server/config called passport.js

---
+++ server/config/passport.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+var passport = require('passport');
+var passportLocal = require('passport-local');
+var student = mongoose.model('student');
+//teach passport how to verify local studentEmail and credentials
+passport.use(new passportLocal.Strategy(
+  { usernameField: 'email',
+    passwordField: 'password'
+  },
+  function(username, password, done){
+  console.log('passport studentEmail received', username);
+  console.log('passport password received', password);
+  student.findOne({ email: username},
+    function (err, user) {
+  console.log('AM I IN', user);
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.validPassword(password)){return done(null, false)}
+      return done(null, user); //there is a record
+    });
+}));
+
+//teach passport how to serialize and deserialize users
+//passport will invoke this function for us
+passport.serializeUser(function(user, done){
+  console.log('serializing');
+  done(null, user.id);
+});
+passport.deserializeUser(function(id, done){
+  //give the id back when they come back
+  //query database or cache here!
+  console.log('deserializing');
+  // done(null, {id:  id, name: id});
+  console.log(id);
+  student.findOne({_id: id}, function(err, user){
+    console.log(user);
+    done(null, user);
+  });
+});
